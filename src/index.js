867d6c8f32edfeb473ebd0dec3507246ca9ca6f0
Student-Java <energoslav@gmail.com>
2020-03-03 00:51:28 -0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    while (str) {
+        let initialLength = str.length;
+
+        for (const [left, right] of bracketsConfig) {
+            str = str.replace(RegExp(`${escapeRegExpSymbols(left)}${escapeRegExpSymbols(right)}`, 'g'), '');
+        }
+
+        if (initialLength === str.length) {
+            return false;
+        }
+    }
+
+    return true;
 }
+
+let escapeRegExpSymbols = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
