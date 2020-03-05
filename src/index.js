0bfa9a335d16962d872112eccdc74f4806af8083
Student-Java <energoslav@gmail.com>
2020-03-04 21:47:47 -0400

    refactor: alter regexp, code cleanup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-    return isValidString(str, bracketsConfig.map(([l, r]) => [escapeRegExpSymbols(l), escapeRegExpSymbols(r)]));
+    return isValidString(str, bracketsConfig.map(([l, r]) => new RegExp(`${escapeSymbols(l)}[^${l + r}]*${escapeSymbols(r)}`, 'g')));
 }
 
 let isValidString = (str, delimeters) => {
     if (!str) {
         return true;
     }
-    let processedStr = vanishDelimiters(str, delimeters);
+    let processedStr = delimeters.reduce((processedStr, regExp) => processedStr.replace(regExp, ''), str);
     return processedStr === str ? false : isValidString(processedStr, delimeters);
 }
 
-let vanishDelimiters = (str, delimeters) => delimeters.reduce((processedStr, [l, r]) => processedStr.replace(RegExp(`${l}${r}`, 'g'), ''), str);
-
-let escapeRegExpSymbols = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+let escapeSymbols = (str) => str.replace(/[.*+?^${}()|\\]/g, '\\$&');
