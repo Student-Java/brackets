e3056c991ac8f9b2e69c94a76edc7b5a3cddf6b8
Student-Java <energoslav@gmail.com>
2020-03-04 21:49:31 -0400

    refactor: fix typo

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ module.exports = function check(str, bracketsConfig) {
     return isValidString(str, bracketsConfig.map(([l, r]) => new RegExp(`${escapeSymbols(l)}[^${l + r}]*${escapeSymbols(r)}`, 'g')));
 }
 
-let isValidString = (str, delimeters) => {
+let isValidString = (str, delimiters) => {
     if (!str) {
         return true;
     }
-    let processedStr = delimeters.reduce((processedStr, regExp) => processedStr.replace(regExp, ''), str);
-    return processedStr === str ? false : isValidString(processedStr, delimeters);
+    let processedStr = delimiters.reduce((processedStr, regExp) => processedStr.replace(regExp, ''), str);
+    return processedStr === str ? false : isValidString(processedStr, delimiters);
 }
 
 let escapeSymbols = (str) => str.replace(/[.*+?^${}()|\\]/g, '\\$&');
