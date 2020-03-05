0bee67f2f2cc94ab03bf4e42c2a1969f454f706e
Student-Java <energoslav@gmail.com>
2020-03-04 22:34:03 -0400

    refactor: add simple non-regexp solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
+    do {
+        var strBefore = str;
+        for (const [left, right] of bracketsConfig) {
+            str = str.replace(`${left}${right}`, '');
+        }
+    } while (str !== strBefore)
+
+    return !str;
+}
+/*
+module.exports = function check(str, bracketsConfig) {
     return isValidString(str, bracketsConfig.map(([l, r]) => new RegExp(`${escapeSymbols(l)}[^${l + r}]*${escapeSymbols(r)}`, 'g')));
 }
 
@@ -9,5 +20,4 @@ let isValidString = (str, delimiters) => {
     let processedStr = delimiters.reduce((processedStr, regExp) => processedStr.replace(regExp, ''), str);
     return processedStr === str ? false : isValidString(processedStr, delimiters);
 }
-
-let escapeSymbols = (str) => str.replace(/[.*+?^${}()|\\]/g, '\\$&');
+*/
